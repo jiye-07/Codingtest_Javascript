75a8d1927cfa378d66cf974f4d9fab5fb27f598b
JIYE <cg2522@naver.com>
2025-06-17 23:34:48 +0900

    [level 0] Title: 개미 군단, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,8 @@
+function solution(hp) {
+  let general = Math.floor(hp / 5);
+  hp = hp % 5;
+  let soldier = Math.floor(hp / 3);
+  hp = hp % 3;
+  let worker = hp;
+  return general + soldier + worker;
+}
